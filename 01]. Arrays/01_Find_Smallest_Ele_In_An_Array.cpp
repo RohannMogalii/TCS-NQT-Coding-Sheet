b2bc2081c2efcf434f09bc5940533cc09c99ff42
Rohan Mogali <141552712+RohannMogalii@users.noreply.github.com>
2024-02-15 14:12:16 +0530

    Update 01_Find_Smallest_Ele_In_An_Array.cpp

--- 01]. Arrays/01_Find_Smallest_Ele_In_An_Array.cpp
+++ 01]. Arrays/01_Find_Smallest_Ele_In_An_Array.cpp
@@ -18,7 +18,7 @@ Explanation: 5 is the smallest element in the array.
 // Time Complexity: O(n*log(n))
 // Space Complexoty: O(1)
 
-/*
+
 #include<bits/stdc++.h>
 using namespace std;
 int findMin(vector<int> &arr,int n){
@@ -37,7 +37,7 @@ int main(){
     cout<<"Smallest Ele: "<<findMin(arr,n);
     return 0;
 }
-*/
+
 
 // Method 2
 // Time Complexity: O(n)
