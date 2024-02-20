bca13b158a6fd6442452082e1bdd48bc244d24fb
Rohan Mogali <141552712+RohannMogalii@users.noreply.github.com>
2024-02-20 20:07:35 +0530

    Add files via upload

---
+++ 01]. Arrays/15_Find_all_the_non-repeating_Ele_in_an_Array.cpp
@@ -0,0 +1,59 @@
+
+/*
+
+Example 1:
+Input:
+    Nums = [1,2,-1,1,3,1]
+Output:
+    2,-1,3
+Explanation:
+    1 is the only element in the given array which occurs thrice in the array. -1,2,3 occurs only once and hence, these are non-repeating elements of the given array.
+
+Example 2:
+Input:
+    Nums = [1,2,3]
+Output:
+    1,2,3
+Explanation:
+    All elements present in the array occur once. Hence, every element is non-repeating.
+
+*/
+
+// ------------------------------------------------------------------------------
+
+// Method 1
+// Using maps
+// TC: O(n)
+// SC: O(n)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void findNonRepeating(int arr[],int n){
+    map<int,int> mp;
+   
+   for(int i=0;i<n;i++){
+       mp[arr[i]]++;
+   }
+   
+   for(auto i:mp){
+       if(i.second==1){
+            cout<<i.first<<" ";
+       }
+   }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+ 
+    findNonRepeating(arr,n);
+ 
+    return 0;
+}
